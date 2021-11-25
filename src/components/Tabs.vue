b2aebfde49e1bda6ff751852ca795d8f0a919c6c
Erico Juan <erico.pieri@gmail.com>
2021-11-25 01:23:55 -0300

    first commit

---
+++ src/components/Tabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-tabs>
+    <v-tab :to="{ name: 'Loja' }" @click.stop.prevent="tipo = 'Geral'">Todos</v-tab>
+    <v-tab :to="{ name: 'Loja' }" @click.stop.prevent="tipo = 'Feminino'">Feminino</v-tab>
+    <v-tab :to="{ name: 'Loja' }" @click.stop.prevent="tipo = 'Masculino'">Masculino</v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+      tipo: '',
+
+    }
+      
+  },
+
+
+  watch: {
+    tipo(t) {
+      this.$store.dispatch('filtrarCamisetas', t)
+    }
+  },
+}
+</script>
